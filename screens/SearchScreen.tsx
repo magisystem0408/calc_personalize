ece0194c78bed67bc7aee55b3f4646ce87f90226
magisystem0408 <masato.matsudo@gmail.com>
2022-02-25 10:52:40 +0900

    add Data

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import {View} from "react-native";
+import {View, StyleSheet, TextInput} from "react-native";
 
-export const SearchScreen:React.FC =()=>{
-    return(
-        <View>
-
+export const SearchScreen: React.FC = () => {
+    return (
+        <View style={styles.container}>
+            <TextInput/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+})
