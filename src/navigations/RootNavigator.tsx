de39adfcd5cae13b313f8facf52cf1084f502ac9
magisystem0408 <masato.matsudo@gmail.com>
2022-03-07 20:31:43 +0900

    add icon and etc...

--- src/navigations/RootNavigator.tsx
+++ src/navigations/RootNavigator.tsx
@@ -3,13 +3,27 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SearchNavigator } from "./SearchNavigator";
 import { PersonalizeView } from "../screens/PersonalizeView";
-
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 const Tab = createBottomTabNavigator();
+
+const screenOption = ({ route }) => ({
+  tabBarIcon: ({ color, size }) => {
+    let iconName;
+
+    if (route.name === "Home") {
+      iconName = "Home";
+    }
+
+    return (
+      <MaterialCommunityIcons name="feature-search" size={24} color="#06B5D3" />
+    );
+  },
+});
 
 export const RootNavigator: React.FC = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={screenOption}>
         <Tab.Screen
           name="Home"
           component={SearchNavigator}
